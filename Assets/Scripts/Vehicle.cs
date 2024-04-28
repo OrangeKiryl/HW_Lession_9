3608101f4dc329fcdfa411fe253b4100c34cfb4a
OrangeKiryl <165653720+OrangeKiryl@users.noreply.github.com>
2024-04-28 14:57:02 +0300

    Initial commit

---
+++ Assets/Scripts/Vehicle.cs
@@ -0,0 +1,37 @@
+﻿public abstract class Vehicle
+{
+  public string Go();
+}
+
+public class Car : Vehicle
+{
+  public override string Go()
+  {
+    console.WriteLine("VroomVroom");
+  }
+}
+
+public class Ship : Vehicle
+{
+  public override string Go()
+  {
+    console.WriteLine("DooDoo");
+  }
+}
+
+public class Plane : Vehicle, IFly
+{
+  public override string Go()
+  {
+    console.WriteLine("Whoooooosh");
+  }
+  public string Fly()
+  {
+    console.WriteLine("Flyyyyyyyyyy");
+  }
+}
+
+public interface IFly
+{
+  string Fly();
+}
